80e4418d9c922682d69c9daf310a1f7110b9137d
akash sunar <sunarakash1122@gmail.com>
2023-07-18 15:29:59 +0545

    creating-props

--- pure-react/src/App.js
+++ pure-react/src/App.js
@@ -1,16 +1,16 @@
-const myFrnd = () => {
-    return React.createElement("div", {}, [("h1", { class: "friend" }, "I am your friend"),
-        React.createElement("h2", { class: "bestFrn" }, "I am your best friend"),
-        React.createElement("h2", { class: "enemy" }, "I am your Enemy")])
+const hero= (props) => {
+    return React.createElement("div", {}, [React.createElement("h1", {},props.name),
+        React.createElement("h2", {},props.age),
+        React.createElement("h3", {},props.hitMovie)])
         
      
 }
 const App = () => {
     return React.createElement("div", {}, [
-        React.createElement("h1",{},"hello SRK fan"),
-        React.createElement(myFrnd),
-         React.createElement(myFrnd),
-        React.createElement(myFrnd)
+        React.createElement("h1",{},"The top khans of Bollywood"),
+        React.createElement(hero, { name:"SRK",age:54,hitMovie:"Pathaan"}),
+         React.createElement(hero, { name:"Salman",age:52,hitMovie:"Bajrangi Bhaijaan"}),
+        React.createElement(hero, { name:"Amir khan",age:50,hitMovie:"PK"})
     ]
     )
 }
