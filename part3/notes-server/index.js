6bfb194ca8ba87acbf6b941ce4b4b33fd0e55ea9
akash sunar <sunarakash1122@gmail.com>
2023-08-10 23:48:07 +0545

    feat: .env works

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,78 +1,104 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require('mongoose')
+require("dotenv").config()
+
+
+const url =process.env.MONGODB_URI
+  
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"))
 
-// const requestLogger = 
+let notes = []
 
-let notes = [
-  {
-    id: 1,
-    content: "Data structure and algorithm",
-    important: true
-  },
-  {
-    id: 2,
-    content: "electronics circuit device",
-    important: false
-  },
-  {
-    id: 3,
-    content: "wireless communications",
-    important: true
-  }
-]
-// app.get("/",(request, response) => {
-//     response.send("<h1>hello akash</h1>");
-// })
-
-
-app.get("/api/notes",(request, response) => {
-    response.json(notes);
-})
-app.get("/api/notes/:id", (request, response) => {
-    const myId = Number(request.params.id);
-    const myNote = notes.find(note => note.id === myId)
-    myNote ? response.json(myNote) : response.status(404).send(`there is no note in ${myId}`)
-});
-app.delete("/api/notes/:id", (request, response) => {
-    const myId = Number(request.params.id);
-    notes = notes.filter(note => note.id !== myId)
-    response.status(204).send(`the note in id ${myId} is deleted`);
-});
-app.post('/api/notes', (request, response) => {
-    const myNewPost = request.body;
-    myNewPost.id = notes.length + 1;
-    notes.push(myNewPost);
-    response.status(201).json(myNewPost)
-})
-
-app.put("/api/notes/:id", (request, response) => {
-  const myId = Number(request.params.id);
-  const updatedNOte = request.body;
-  let noteFound = false;
-  notes = notes.map((note) => {
-    if (note.id !== myId) return note;
-    else {
-      noteFound = true;
-      return updatedNOte;
-    }
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((result)=>{response.json(result)})
+    })
+app.get("/api/notes/:id", (request, response,next) => {
+  Note.findById(request.params.id).then((result) => {
+    result ? response.json(result) : response.status(404).send(`there is no note in ${request.params.id}`)
+    
+  }).catch(e => {
+    next(e);
   })
-  
-  if (noteFound) {
-    response.status(202).json(updatedNOte)
-  } else {
-    response.status(404).send(`there is no note in ${myId}`)
-  }
 });
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 app.use((request, response, next) => {
   response.status(404).send("no url available")
   next()
 }
 )
-const PORT = process.env.PORT?process.env.PORT: 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
