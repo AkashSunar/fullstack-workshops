ddf56ff97e1c0c2683a5117afec164ccfcd9817f
akash sunar <sunarakash1122@gmail.com>
2023-09-06 14:00:57 +0545

    adding functionality  and acton creater in searate folder

---
+++ part6/redux-notes/src/reducers/noteReducer.js
@@ -0,0 +1,34 @@
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case "NEW_NOTE": {
+      const newState = state.concat(action.payload);
+      return newState;
+    }
+    case "TOGGLE_IMPORTANCE": {
+      let myNote = state.find((note) => note.id === action.payload.id);
+      let changedNote = { ...myNote, important: !myNote.important };
+      //   changedNote.important = !changedNote.important;
+      return state.map((note) =>
+        note.id === changedNote.id ? changedNote : note
+      );
+    }
+    default:
+      return state;
+  }
+};
+ const createNote = (newNote) => {
+  return {
+    type: "NEW_NOTE",
+    payload: newNote,
+  };
+};
+ const toggleImportantOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: {
+      id,
+    },
+  };
+};
+export { createNote, toggleImportantOf };
+export default noteReducer;
