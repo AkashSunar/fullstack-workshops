ef80a16d40e3e5b741d602b7c6ba199b1226309e
akash sunar <sunarakash1122@gmail.com>
2023-10-05 13:23:51 +0545

    feat: post API done usng rest client

--- part9/ts-project/index.ts
+++ part9/ts-project/index.ts
@@ -1,5 +1,7 @@
+// const express=require("express")
 import express from "express"
 const app = express();
+app.use(express.json())
 import { calculator,Operation} from "./multiplier";
 
 app.get('/ping', (_req, res) => {
@@ -8,8 +10,9 @@ app.get('/ping', (_req, res) => {
 
 app.post('/calculate', (req, res) => {
     const { value1, value2, op } = req.body;
+    console.log(req.body,"checking req body") 
     const operation = op as Operation;
-    const result = calculator(Number(value1), Number(value2),operation);
+    const result = calculator(Number(value1),Number(value2),operation);
   res.send({ result });
 });
 
